29a4f5ae0246d971cc9723532707f54f44c02a8f
croben <crobenintern@gmail.com>
2021-03-22 18:46:07 +0800

    Version 0.1

---
+++ QDCrypt/QDCrypt.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace QDCrypt {
+    partial class QDCrypt {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.txtCipher = new System.Windows.Forms.TextBox();
+            this.txtHashKey = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtCipher
+            // 
+            this.txtCipher.Location = new System.Drawing.Point(3, 5);
+            this.txtCipher.Multiline = true;
+            this.txtCipher.Name = "txtCipher";
+            this.txtCipher.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtCipher.Size = new System.Drawing.Size(296, 100);
+            this.txtCipher.TabIndex = 0;
+            this.txtCipher.Text = "Input your text or cipher.";
+            // 
+            // txtHashKey
+            // 
+            this.txtHashKey.Location = new System.Drawing.Point(58, 113);
+            this.txtHashKey.Name = "txtHashKey";
+            this.txtHashKey.Size = new System.Drawing.Size(185, 20);
+            this.txtHashKey.TabIndex = 3;
+            this.txtHashKey.Text = "Hash Key";
+            this.txtHashKey.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(3, 111);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(54, 23);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(245, 111);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(54, 23);
+            this.btnDecrypt.TabIndex = 4;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(3, 140);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(296, 100);
+            this.txtResult.TabIndex = 5;
+            this.txtResult.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtResult_KeyDown);
+            // 
+            // QDCrypt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(304, 246);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtHashKey);
+            this.Controls.Add(this.txtCipher);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "QDCrypt";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QDCrypt 0.1";
+            this.Load += new System.EventHandler(this.QDCrypt_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCipher;
+        private System.Windows.Forms.TextBox txtHashKey;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ QDCrypt/QDCrypt.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QDCrypt {
+    public partial class QDCrypt : Form {
+        public QDCrypt() {
+            InitializeComponent();
+        }
+
+        private void QDCrypt_Load(object sender, EventArgs e) {
+
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e) {
+
+            string hashKey = GetSHA256Hash(txtHashKey.Text);
+            string key = hashKey.Substring(0, 32);
+            string iv = hashKey.Substring(hashKey.Length - 16);
+
+            txtResult.Text = AesEcnrypt(txtCipher.Text.Trim(), key, iv);
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e) {
+
+            string hashKey = GetSHA256Hash(txtHashKey.Text);
+            string key = hashKey.Substring(0, 32);
+            string iv = hashKey.Substring(hashKey.Length - 16);
+
+            txtResult.Text = AesDecrypt(txtCipher.Text.Trim(), key, iv);
+        }
+
+        // Make Ctrl+A shortcut work on read only textbox
+        private void txtResult_KeyDown(object sender, KeyEventArgs e) {
+            if (e.Control && e.KeyCode == Keys.A) {
+                txtResult.SelectAll();
+            }
+        }
+
+        private string GetSHA256Hash(string dataToHash) {
+            // Setup SHA
+            SHA256Managed crypt = new SHA256Managed();
+            string hash = string.Empty;
+
+            // Compute hash
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(dataToHash), 0, Encoding.UTF8.GetByteCount(dataToHash));
+
+            // Convert to hex
+            foreach (byte bit in crypto)
+                hash += bit.ToString("x2");
+
+            return hash;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <param name="key"></param> 32 character long string = 256 bits
+        /// <param name="iv"></param> 16 character long string = 128 bits
+        /// <returns></returns>
+        private string AesEcnrypt(string plainText, string key, string iv) {
+
+            byte[] textBytes = ASCIIEncoding.ASCII.GetBytes(plainText);
+
+            AesCryptoServiceProvider crypt = new AesCryptoServiceProvider {
+                BlockSize = 128,
+                KeySize = 256,
+                Key = ASCIIEncoding.ASCII.GetBytes(key),
+                IV = ASCIIEncoding.ASCII.GetBytes(iv),
+                Padding = PaddingMode.PKCS7,
+                Mode = CipherMode.CBC
+            };
+
+            ICryptoTransform icrypt = crypt.CreateEncryptor(crypt.Key, crypt.IV);
+
+            byte[] result = icrypt.TransformFinalBlock(textBytes, 0, textBytes.Length);
+            icrypt.Dispose();
+
+            return Convert.ToBase64String(result);
+        }
+
+        private string AesDecrypt(string cipher, string key, string iv) {
+
+            byte[] cipherBytes;
+
+            // Catch errors if cipher is not a Base64String.
+            try {
+                cipherBytes = Convert.FromBase64String(cipher);
+            } catch (Exception) {
+
+                return cipher;
+            }
+
+            AesCryptoServiceProvider crypt = new AesCryptoServiceProvider {
+                BlockSize = 128,
+                KeySize = 256,
+                Key = ASCIIEncoding.ASCII.GetBytes(key),
+                IV = ASCIIEncoding.ASCII.GetBytes(iv),
+                Padding = PaddingMode.PKCS7,
+                Mode = CipherMode.CBC
+            };
+
+            ICryptoTransform icrypt = crypt.CreateDecryptor(crypt.Key, crypt.IV);
+            byte[] result;
+
+            // Catch errors if the decryption failed.
+            try {
+                result = icrypt.TransformFinalBlock(cipherBytes, 0, cipherBytes.Length);
+                icrypt.Dispose();
+            } catch (Exception) {
+
+                return cipher;
+            }
+            
+            return ASCIIEncoding.ASCII.GetString(result);
+        }
+    }
+}
